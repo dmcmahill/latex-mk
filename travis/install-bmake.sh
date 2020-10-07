c58d9c3f3b44807d62258129b0d482f880be3f2b
Dan McMahill <dan@mcmahill.net>
2020-10-06 22:34:39 -0400

    [travis] do not fail on bmake internal tests

--- travis/install-bmake.sh
+++ travis/install-bmake.sh
@@ -8,6 +8,11 @@ set -ex
 wget http://www.crufty.net/ftp/pub/sjg/${bmake_dist} -O ${bmake_dist}
 tar -zxvf ${bmake_dist}
 cd ${bmake_nm}
+
+mv boot-strap boot-strap.orig
+sed 's/Bmake test \|\| exit 1/Bmake test/g' boot-strap.orig > boot-strap
+chmod 755 boot-strap
+
 ./configure --prefix=/usr
 make
 sudo make install
