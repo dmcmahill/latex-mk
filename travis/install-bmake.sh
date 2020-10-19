874a6ae060fc6dadbbf5a45bf9a7db9f6889a5a7
Dan McMahill <dan@mcmahill.net>
2020-10-19 17:08:01 -0400

    [travis] Recent bmake update has new unit tests, some fail.  Add some needed deps and add some script flexibility

--- travis/install-bmake.sh
+++ travis/install-bmake.sh
@@ -1,18 +1,79 @@
 #!/bin/sh
 
-#bmake_ver=4.2.5
+bmake_prefix=${bmake_prefix:-/usr}
+
 bmake_nm=bmake
 bmake_dist=${bmake_nm}.tar.gz
+bmake_url=http://www.crufty.net/ftp/pub/sjg/${bmake_dist}
+
+do_install=yes
+usage() {
+    cat <<EOF
+
+$0 - download and install BSD make (bmake)
+
+Options
+
+    -h|--help       : show this help message and exit
+
+    --no-install    :  build but do not install
+
+    --prefix prefix : specify an install prefix.  Default is ${bmake_prefix}
+
+EOF
+}
+
+while test $# -gt 0 ; do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+
+        --no-install)
+            do_install=no
+            shift
+            ;;
+
+        --prefix)
+            bmake_prefix=$2
+            shift 2
+            ;;
+
+        -*)
+            echo "$1 not understood"
+            exit 1
+            ;;
+
+        *)
+            break
+            ;;
+    esac
+done
 
 set -ex
-wget http://www.crufty.net/ftp/pub/sjg/${bmake_dist} -O ${bmake_dist}
+if test -x /usr/bin/wget ; then
+    wget ${bmake_url} -O ${bmake_dist}
+else
+    ftp ${bmake_url}
+fi
+
 tar -zxvf ${bmake_dist}
 cd ${bmake_nm}
 
-for f in `find ../travis/ -name bmake-patch\* -print` ; do
-    echo "Apply $f"
-    patch -p0 < $f
-done
+echo "Checking VERSION"
+cat VERSION
+
+
+PATCH_DIR=../travis
+PATCH_FILE_BASE=bmake-patch
+if ls ${PATCH_DIR}/${PATCH_FILE_BASE}* 2>/dev/null ; then
+    echo "Applying local patches"
+    for f in `find ${PATCH_DIR}/ -name ${PATCH_FILE_BASE}\* -print` ; do
+        echo "Apply $f"
+        patch -p0 < $f
+    done
+fi
 
 cat << EOF
 
@@ -22,13 +83,15 @@ DEFAULT_SYS_PATH : ${DEFAULT_SYS_PATH}
 
 EOF
 
-./configure --prefix=/usr 
-make prefix=/usr DEFAULT_SYS_PATH=/usr/share/mk
-sudo make install
+./configure --prefix=${bmake_prefix}
+make prefix=${bmake_prefix} DEFAULT_SYS_PATH=${bmake_prefix}/share/mk
 
-echo "Running basic sanity check for functionality"
+if test $do_install = yes ; then
+    sudo make install
 
-cat > tmp.mk <<EOF
+    echo "Running basic sanity check for functionality"
+
+    cat > tmp.mk <<EOF
 # include some of the "." commands that we need from a 
 # BSD make.
 MYVAR= BSDmake
@@ -41,9 +104,11 @@ test:
 .endfor
 EOF
 
-echo "========= tmp.mk =========="
-cat tmp.mk
-echo "========= ====== =========="
+    echo "========= tmp.mk =========="
+    cat tmp.mk
+    echo "========= ====== =========="
 
-/usr/bin/bmake -f tmp.mk
+    ${bmake_prefix}/bin/bmake -f tmp.mk
+
+fi
 
