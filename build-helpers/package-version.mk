b5a8774db7dd7f95380941b5f3af1121fc56e539
Dan McMahill <dan@mcmahill.net>
2025-05-23 19:08:01 -0400

    [build] Fix up some version checks and NEWS file checks

--- build-helpers/package-version.mk
+++ build-helpers/package-version.mk
@@ -1,14 +1,19 @@
+# The directory that holds this file relative to $(top_srcdir)
 BUILD_SCRIPT_DIR = build-helpers
 
 # Check that package version matches git version before creating dist tarballs
-dist-hook: git-version-check git-version-check-news git-version-stamp
+# May want to add git-version-check-news back in.
+dist-hook: git-version-check git-version-stamp
 distcheck-hook: git-version-check
 
 # Note: We cannot run autogen.sh from here, because we would need some way to
 #       restart the whole dist process from the start and there is none.
-EXTRA_DIST += $(top_srcdir)/$(BUILD_SCRIPT_DIR)/package-version.sh
+PACKAGE_VERSION_SH = $(top_srcdir)/$(BUILD_SCRIPT_DIR)/package-version.sh
+EXTRA_DIST += $(PACKAGE_VERSION_SH)
+EXTRA_DIST += $(top_srcdir)/$(BUILD_SCRIPT_DIR)/package-version.mk
+.PHONY: git-version-check
 git-version-check:
-	@git_ver=`$(top_srcdir)/$(BUILD_SCRIPT_DIR)/package-version.sh $(top_srcdir) version-stamp`; \
+	@git_ver=$$($(PACKAGE_VERSION_SH) $(top_srcdir) version-stamp); \
 	if test "x$${git_ver}" = "x$(PACKAGE_VERSION)"; then :; else \
 		echo "ERROR: PACKAGE_VERSION and 'git describe' version do not match:"; \
 		echo "         current 'git describe' version: $${git_ver}"; \
@@ -24,28 +29,36 @@ git-version-check:
 
 # FIXME: NEWS check uses ${foo%%-*} POSIX shell, tested
 #        with bash, dash, busybox.
+.PHONY: git-version-check-news
 git-version-check-news:
-	@git_ver=`$(top_srcdir)/$(BUILD_SCRIPT_DIR)/package-version.sh $(top_srcdir) version-stamp`; \
-	gv_xyz="$${git_ver%%-*}"; \
+	@git_ver=$$($(PACKAGE_VERSION_SH) $(top_srcdir) version-stamp); \
+	gv_xyz="$$(echo $${git_ver} | $(AWK) -F '[^0-9]' '{maj=$$1; min=$$2; patch=$$3;} END{printf("%d.%d.%d\n", maj, min, patch)}')" ; \
+	if [[ "$${git_ver}" != "$${gv_xyz}" ]] ; then is_release=no; else is_release=yes ; fi ; \
 	gv_xy="$${gv_xyz%.*}"; \
-	case `$(SED) 1q $(top_srcdir)/NEWS` in \
-	"$(PACKAGE_TARNAME) $${gv_xyz}") : ;; \
-	"$(PACKAGE_TARNAME) $${gv_xy}.x") : ;; \
-	*) \
-	  echo "NEWS not updated for version $${git_ver%%-*}; not releasing" 1>&2; \
-	  exit 1;; \
-	esac
+	pkg="$$(echo "$(PACKAGE_TARNAME)" | $(AWK) -f $(top_srcdir)/$(BUILD_SCRIPT_DIR)/pascal-case.awk)" ; \
+	echo "Checking for $${pkg}-$${gv_xyz} or $${pkg}-$${gv_xy} release notes" ; \
+	updated=no ; \
+	if grep -q "^Release Notes for $${pkg}-$${gv_xyz}$$" "$(top_srcdir)/NEWS" ; then updated=yes ; fi ; \
+	if grep -q "^Release Notes for $${pkg}-$${gv_xy}$$" "$(top_srcdir)/NEWS" ; then updated=yes ; fi ; \
+	if [[ $${updated} = "yes" ]] ; then \
+		echo "NEWS is updated for version $${git_ver} ($${gv_xyz} or $${gv_xy})" ; \
+	else \
+		echo "NEWS not updated for version $${git_ver} ($${gv_xyz} or $${gv_xy}) not releasing" 1>&2; \
+		exit 1; \
+	fi
 
 # Version stamp files can only exist in tarball source trees.
 #
 # So there is no need to generate them anywhere else or to clean them
 # up anywhere.
+.PHONY: git-version-stamp
 git-version-stamp:
 	echo "$(PACKAGE_VERSION)" > "$(distdir)/version-stamp"
 
 # Requires git 1.5 to work properly.
-if HAVE_GIT
+#if HAVE_GIT
 # Usage: $ make tag VER=1.2
+.PHONY: tag
 tag:
 	test -d "$(top_srcdir)/.git"
 	@cd "$(top_srcdir)" && $(GIT) status | cat;:
@@ -76,14 +89,14 @@ tag:
 	msgfile="$$PWD/TAG-MESSAGE"; \
 	cd "$(top_srcdir)" && $(GIT) tag -s -F "$$msgfile" "$(PACKAGE_TARNAME)-$(VER)"; \
 	rm -f "$$msgfile"
-endif
+#endif
 
 # Update *.h file to contain up-to-date version number
 A_V = package-version-internal
 CLEANFILES    += $(A_V).h
 BUILT_SOURCES += $(A_V).h.stamp
 $(A_V).h.stamp:
-	@current_ver=`$(SHELL) $(top_srcdir)/$(BUILD_SCRIPT_DIR)/package-version.sh $(top_srcdir) version-stamp`; \
+	@current_ver=$$($(PACKAGE_VERSION_SH) $(top_srcdir) version-stamp); \
 	{ echo '#ifndef PACKAGE_VERSION_INTERNAL_H'; \
 	  echo "#define PACKAGE_VERSION_INTERNAL \"$${current_ver}\""; \
 	  echo "#endif /* !PACKAGE_VERSION_INTERNAL */"; } > "$(A_V).h.new"
